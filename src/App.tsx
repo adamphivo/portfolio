54881c2babaa3d00736c5a1fdf604296fb6b2ee8
adamphivo <adamphivo@gmail.com>
2023-05-18 15:28:32 +0200

    ✨ - Add smooth scroll using rolly.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,37 @@
+import { useEffect } from "react";
 import Hero from "./components/Hero/Hero";
 import Projects from "./components/Projects/Projects";
 import Contact from "./components/Contact/Contact";
 import Tools from "./components/Tools/Tools";
 import Grain from "./components/Grain";
+import rolly from "rolly.js";
 
 function App() {
+  useEffect(() => {
+    const view = document.querySelector(".app");
+    const r = rolly({
+      view,
+      native: true,
+      // other options
+    });
+    r.init();
+  }, []);
   return (
     <>
       <Grain />
-      <main>
-        <Hero />
-        <Projects />
-        <Tools />
-        <Contact />
+      <main className="app">
+        <div data-scene>
+          <Hero />
+        </div>
+        <div data-scene>
+          <Projects />
+        </div>
+        <div data-scene>
+          <Tools />
+        </div>
+        <div data-scene>
+          <Contact />
+        </div>
       </main>
     </>
   );
